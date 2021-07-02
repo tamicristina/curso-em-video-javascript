8a2ada3a2b598264d757de369ca21ab55d622f7f
tamicristina <tami.cristina06@gmail.com>
2021-07-02 19:34:50 -0300

    Iniciando exercício

---
+++ ex017/script.js
@@ -0,0 +1,10 @@
+let numerotxt = document.querySelector("#numero");
+let numero = Number(numerotxt.value);
+console.log(numeroPrimeiro);
+
+let numero = 7;
+
+for (let c = 1; c <= 10; c++) {
+  let contador = numero * c;
+  console.log("7 x " + c + " = " + contador);
+}
