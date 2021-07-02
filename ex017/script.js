7d4295af60a85100f0659921c38d5c05920d320d
tamicristina <tami.cristina06@gmail.com>
2021-07-02 20:51:34 -0300

    Exercício finalizado

--- ex017/script.js
+++ ex017/script.js
@@ -1,10 +1,20 @@
-let numerotxt = document.querySelector("#numero");
-let numero = Number(numerotxt.value);
-console.log(numeroPrimeiro);
+function gerarTabuada() {
+  let numerotxt = document.querySelector("#numero");
+  let tabuada = document.querySelector("#resptab");
+  console.log(tabuada);
 
-let numero = 7;
+  if (numerotxt.value.length == 0) {
+    window.alert("Por favor, digite um numero!");
+  } else {
+    let numero = Number(numerotxt.value);
+    tabuada.innerHTML = "";
 
-for (let c = 1; c <= 10; c++) {
-  let contador = numero * c;
-  console.log("7 x " + c + " = " + contador);
+    for (let c = 1; c <= 10; c++) {
+      let contador = numero * c;
+      let item = document.createElement("option");
+      item.text = `${numero} x ${c} = ${contador}`;
+      item.value = `tab${c}`;
+      tabuada.appendChild(item);
+    }
+  }
 }
