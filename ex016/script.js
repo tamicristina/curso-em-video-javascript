fb6c351cb422c28257de6ce16d8f55618a369b2e
tamicristina <tami.cristina06@gmail.com>
2021-07-01 21:34:52 -0300

    Exercício finalizado

--- ex016/script.js
+++ ex016/script.js
@@ -2,46 +2,36 @@ let inicio = document.querySelector("#inicio");
 let fim = document.querySelector("#fim");
 let passo = document.querySelector("#passo");
 let resposta = document.querySelector("#res");
-let contador = 0; // valor inicial
 
 function contar() {
   resposta.innerHTML = `<p> Contando:</p>`;
   if (
-    inicio.value.length == "" ||
-    fim.value.length == "" ||
-    passo.value.length == ""
+    inicio.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
   ) {
     resposta.innerHTML = `<p> Impossível contar!</p>`;
-  } else if (Number(passo.value) == Number(0)) {
-    window.alert("Passo inválido! Considerando PASSO 1");
-    for (
-      contador = Number(inicio.value);
-      contador <= Number(fim.value);
-      contador++
-    ) {
-      resposta.innerHTML += ` ${contador} &#128073`;
+  } else {
+    let p = Number(passo.value);
 
-      console.log(contador);
+    if (p <= 0) {
+      window.alert("Passo inválido! Considerando PASSO 1");
+      p = 1;
+      console.log(p);
     }
-  }
-
-  if (Number(passo.value) > 0) {
-    for (
-      contador = Number(inicio.value);
-      contador <= Number(fim.value);
-      contador += Number(passo.value)
-    ) {
-      resposta.innerHTML += ` ${contador} &#128073`;
+    if (Number(inicio.value) < Number(fim.value)) {
+      for (let c = Number(inicio.value); c <= Number(fim.value); c += p) {
+        resposta.innerHTML += ` ${c} &#128073`;
+      }
+    } else {
+      for (
+        let c = Number(inicio.value);
+        c >= Number(fim.value);
+        c -= Number(passo.value)
+      ) {
+        resposta.innerHTML += ` ${c} &#128073`;
+      }
     }
-  }
-
-  if (Number(inicio.value) > Number(fim.value)) {
-    for (
-      contador = Number(inicio.value);
-      contador >= Number(fim.value);
-      contador -= Number(passo.value)
-    ) {
-      resposta.innerHTML += ` ${contador} &#128073`;
-    }
+    resposta.innerHTML += `\u{1F3C1}`;
   }
 }
